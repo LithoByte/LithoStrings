f1f58137da992f13325a1a16d5a4ca7155034808
Remmington Damper <kincaiddamper@gmail.com>
2022-02-25 08:28:01 -0500

    fixed compiler error

--- LithoStrings/Classes/NSAttributesReturnFunctions.swift
+++ LithoStrings/Classes/NSAttributesReturnFunctions.swift
@@ -8,67 +8,67 @@
 import Foundation
 import UIKit
 
-    public func foregroundColor(color: UIColor) -> [NSAttributedString.Key: Any] {
+    public func foregroundColorReturner(color: UIColor) -> [NSAttributedString.Key: Any] {
         return [.foregroundColor: color]
     }
-    public func attachment(attachment: NSTextAttachment) -> [NSAttributedString.Key: Any] {
-        return [.attachment: attachment]
+    public func attachmentReturner(forAttachment: NSTextAttachment) -> [NSAttributedString.Key: Any] {
+        return [.attachment: forAttachment]
     }
-    public func backgroundColor(color: UIColor) -> [NSAttributedString.Key: Any] {
+    public func backgroundColorReturner(color: UIColor) -> [NSAttributedString.Key: Any] {
         return [.backgroundColor: color]
     }
-    public func baselineOffset(offset: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func baselineOffsetReturner(offset: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.baselineOffset: offset]
     }
-    public func expansion(expansion: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func expansionReturner(expansion: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.expansion: expansion]
     }
-    public func font(font: UIFont) -> [NSAttributedString.Key: Any] {
+    public func fontReturner(font: UIFont) -> [NSAttributedString.Key: Any] {
         return [.font: font]
     }
-    public func kern(kern: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func kernReturner(kern: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.kern: kern]
     }
-    public func ligature(ligature: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func ligatureReturner(ligature: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.ligature: ligature]
     }
-    public func link(link: NSURL) -> [NSAttributedString.Key: Any] {
+    public func linkReturner(link: NSURL) -> [NSAttributedString.Key: Any] {
         return [.link: link]
     }
-    public func obliqueness(obliqueness: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func obliquenessReturner(obliqueness: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.obliqueness: obliqueness]
     }
-    public func paragraphStyle(style: NSParagraphStyle) -> [NSAttributedString.Key: Any] {
+    public func paragraphStyleReturner(style: NSParagraphStyle) -> [NSAttributedString.Key: Any] {
         return [.paragraphStyle: style]
     }
-    public func shadow(shadow: NSShadow) -> [NSAttributedString.Key: Any] {
+    public func shadowReturner(shadow: NSShadow) -> [NSAttributedString.Key: Any] {
         return [.shadow: shadow]
     }
-    public func strikethroughColor(color: UIColor) -> [NSAttributedString.Key: Any] {
+    public func strikethroughColorReturner(color: UIColor) -> [NSAttributedString.Key: Any] {
         return [.strikethroughColor: color]
     }
-    public func strikethroughStyle(style: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func strikethroughStyleReturner(style: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.strikethroughStyle: style]
     }
-    public func strokeColor(color: UIColor) -> [NSAttributedString.Key: Any] {
+    public func strokeColorReturner(color: UIColor) -> [NSAttributedString.Key: Any] {
         return [.strokeColor: color]
     }
-    public func strokeWidth(width: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func strokeWidthReturner(width: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.strokeWidth: width]
     }
-    public func textEffect(effect: NSString) -> [NSAttributedString.Key: Any] {
+    public func textEffectReturner(effect: NSString) -> [NSAttributedString.Key: Any] {
         return [.textEffect: effect]
     }
-    public func underlineColor(color: UIColor) -> [NSAttributedString.Key: Any] {
+    public func underlineColorReturner(color: UIColor) -> [NSAttributedString.Key: Any] {
         return [.underlineColor: color]
     }
-    public func underlineStyle(style: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func underlineStyleReturner(style: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.underlineStyle: style]
     }
-    public func verticalGlyphForm(form: NSNumber) -> [NSAttributedString.Key: Any] {
+    public func verticalGlyphFormReturner(form: NSNumber) -> [NSAttributedString.Key: Any] {
         return [.verticalGlyphForm: form]
     }
-    public func writingDirection(direction: NSArray) -> [NSAttributedString.Key: Any] {
+    public func writingDirectionReturner(direction: NSArray) -> [NSAttributedString.Key: Any] {
         return [.writingDirection: direction]
     }
 
--- LithoStrings/Classes/NSMutableAttributeStringAdds.swift
+++ LithoStrings/Classes/NSMutableAttributeStringAdds.swift
@@ -8,108 +8,66 @@
 import Foundation
 import UIKit
 
-    public func setForegroundColorAttributes(color: UIColor, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key:Any] = [:]
-        addForegroundColorToAttributes(color: color, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+    public func setForegroundColorAttributes(color: UIColor, string: NSMutableAttributedString){
+        string.addAttributes(foregroundColorReturner(color: color), range: NSMakeRange(0, string.length))
     }
-    public func setAttachmentAttributes(attachment: NSTextAttachment, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addAttachmentAttributes(attachment: attachment, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
-}
+    public func setAttachmentAttributes(attachment: NSTextAttachment, string: NSMutableAttributedString){
+        string.addAttributes(attachmentReturner(forAttachment: attachment), range: NSMakeRange(0, string.length))
+    }
     public func setbackgroundColorAttributes(color: UIColor, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addBackgroundColorToAttributes(color: color, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(backgroundColorReturner(color: color), range: NSMakeRange(0, string.length))
     }
     public func setBaselineoffsetAttributes(offset: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addBaselineOffsetToAttributes(offset: offset, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(baselineOffsetReturner(offset: offset), range: NSMakeRange(0, string.length))
     }
     public func setExpansionAttributes(expansion: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addExpansionToAttributes(expansion: expansion, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(expansionReturner(expansion: expansion), range: NSMakeRange(0, string.length))
     }
     public func setFontAttributes(font: UIFont, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addFontToAttributes(font: font, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(fontReturner(font: font), range: NSMakeRange(0, string.length))
     }
     public func setKernAttributes(kern: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addKernToAttributes(kern: kern, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(kernReturner(kern: kern), range: NSMakeRange(0, string.length))
     }
     public func setLigatureAttributes(ligature: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addLigatureToAttributes(ligature: ligature, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(ligatureReturner(ligature: ligature), range: NSMakeRange(0, string.length))
     }
     public func setLinkAttributes(link: NSURL, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addLinkToAttributes(link: link, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(linkReturner(link: link), range: NSMakeRange(0, string.length))
     }
     public func setObliquenessAttributes(obliqueness: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addObliquenessToAttributes(obliqueness: obliqueness, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(obliquenessReturner(obliqueness: obliqueness), range: NSMakeRange(0, string.length))
     }
     public func setParagraphStyleAttributes(style: NSParagraphStyle, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addParagraphStyleToAttributes(paragraphStyle: style, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(paragraphStyleReturner(style: style), range: NSMakeRange(0, string.length))
     }
     public func setShadowAttributes(shadow: NSShadow, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addShadowToAttributes(shadow: shadow, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(shadowReturner(shadow: shadow), range: NSMakeRange(0, string.length))
     }
     public func setStrikethroughColorAttributes(color: UIColor, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addStrikeThroughColorToAttributes(color: color, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(strokeColorReturner(color: color), range: NSMakeRange(0, string.length))
     }
     public func setStrikethroughStyleAttributes(style: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addStrikeThroughStyleToAttributes(style: style, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(strikethroughStyleReturner(style: style), range: NSMakeRange(0, string.length))
     }
     public func setStrokeColorAttributes(color: UIColor, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addStrokeColorToAttributes(color: color, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(strokeColorReturner(color: color), range: NSMakeRange(0, string.length))
     }
     public func setStrokeWidthAttributes(width: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addStrokeWidthToAttributes(width: width, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(strokeWidthReturner(width: width), range: NSMakeRange(0, string.length))
     }
     public func setTextEffectAttributes(effect: NSString, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addTextEffectToAttributes(effect: effect, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(textEffectReturner(effect: effect), range: NSMakeRange(0, string.length))
     }
     public func setUnderlineColorAttributes(color: UIColor, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addUnderlineColorToAttributes(color: color, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(underlineColorReturner(color: color), range: NSMakeRange(0, string.length))
     }
     public func setUnderlineStyleAttributes(style: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addUnderlineStyleToAttributes(style: style, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(underlineStyleReturner(style: style), range: NSMakeRange(0, string.length))
     }
     public func setVerticalGlyphFormAttributes(style: NSNumber, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addVerticalGlyphFormToAttributes(style: style, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(verticalGlyphFormReturner(form: style), range: NSMakeRange(0, string.length))
     }
     public func setWritingDirectionAttributes(direction: NSArray, string: NSMutableAttributedString) -> Void {
-        var attributes: [NSAttributedString.Key: Any] = [:]
-        addWritingDirectionToAttributes(direction: direction, attributes: &attributes)
-        string.addAttributes(attributes, range: NSMakeRange(0, string.length))
+        string.addAttributes(writingDirectionReturner(direction: direction), range: NSMakeRange(0, string.length))
     }
